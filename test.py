09decf193a80cab8d2adb0531aa8f829561300c7
Sam Nicholls <samstudio8@gmail.com>
2021-01-31 16:34:34 +0000

    publish: Try out new API docs style
    generated from commit de10328196d6503559218ae8e34548b43d4bd200

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
+import sys
+
 from prance import ResolvingParser, BaseParser
 parser = ResolvingParser('api/majora.yaml')
 spec = parser.specification  # contains fully resolved specs as a dict
+
+
+PRIORITY_COLOUR = {
+    0: "color:#fff; background-color:#dc3545",
+    1: "color:#000; background-color:#ffc107",
+    2: "color:#fff; background-color:#17a2b8",
+    None: "color:#fff; background-color:#6c757d",
+}
+PRIORITY_NAME = {
+    0: "required",
+    1: "possibly required",
+    2: "recommended",
+    None: "",
+}
 
 def flatten_object(spec, prefix=""):
     flat_spec = {}
@@ -16,16 +32,25 @@ def flatten_object(spec, prefix=""):
             if field_spec.get("type") == "array":
                 subspec = flatten_object(field_spec.get("items", {}), prefix+field+'.')
                 flat_spec.update(subspec)
+            elif field_spec.get("type") == "object":
+                subspec = flatten_object(field_spec, prefix+field+'.')
+                flat_spec.update(subspec)
             else:
                 flat_spec[field] = {
                     "path": prefix+field,
                     "name": field,
                     "type": field_spec.get("type"),
                     "required": field in sspec.get("required", []),
+                    "x-priority": field_spec.get("x-priority", 100),
                     "description": field_spec.get("description", ""),
                     "enum": [x if x else "(blank)" for x in field_spec.get("enum", [])],
                     "example": field_spec.get("example"),
                     "x-ocarina-param": field_spec.get("x-ocarina-param", "NA"),
+                    "x-ocarina-warning": field_spec.get("x-ocarina-warning", ""),
+                    "x-ocarina-namespace": field_spec.get("x-ocarina-namespace", ""),
+                    "x-ocarina-in-record": field_spec.get("x-ocarina-in-record", False),
+                    "x-uploader-column": field_spec.get("x-uploader-column"),
+                    "x-uploader-limit": field_spec.get("x-uploader-limit"),
                 }
     return flat_spec
 
@@ -34,9 +59,10 @@ for path, spec in spec["paths"].items():
     #print(spec['post']['tags'])
 
     print("## %s" % spec['post']['summary'])
-    print("<code>%s</code>" % path)
 
-    print("### Attributes")
+    print("\n<code>%s</code>" % path)
+
+    print("\n\n### Attributes")
     cmd_head = "ocarina " + spec['post'].get('x-ocarina-cmd', "")
     cmd = []
     cmd_req = []
@@ -44,8 +70,11 @@ for path, spec in spec["paths"].items():
     for k, v in spec['post']['requestBody']['content']['application/json']['schema'].items():
         for item in v:
             flat = flatten_object(item)
-            sort_flat = sorted(flat.items(), key=lambda x: (not x[1].get("required", False), x[1].get("name")))
+            sort_flat = sorted(flat.items(), key=lambda x: (x[1].get("x-priority", 100), x[1].get("name")))
             for kp, vp in sort_flat:
+                if "metric" in vp["path"] or "metadata" in vp["path"]:
+                    continue
+
                 if vp.get("x-ocarina-param") == "NA":
                     pass
                 elif vp.get("x-ocarina-param") is None:
@@ -103,11 +132,125 @@ for path, spec in spec["paths"].items():
         print("\nName | Description | Options")
         print("---- | ----------- | -------")
         for kp, vp in sort_flat:
+            if "metric" in vp["path"] or "metadata" in vp["path"]:
+                continue
+
+            pri_colour = PRIORITY_COLOUR.get(vp["x-priority"])
+            if not pri_colour:
+                pri_colour = PRIORITY_COLOUR.get(None)
+            name = "<b><code%s>%s</code></b>" % (" style='%s'" % pri_colour, vp["name"])
+            field_description = "%s%s%s" % (vp["type"], ", <i>%s</i>" % PRIORITY_NAME.get(vp["x-priority"]) if PRIORITY_NAME.get(vp["x-priority"]) else "", ", <i>enum</i>" if len(vp.get("enum")) > 0 else "")
+
+            aside = ""
+            if vp.get("x-ocarina-warning"):
+                aside = "<aside class='warning' style='padding: 1em'>%s</aside>" % vp["x-ocarina-warning"]
+
             print(' | '.join([str(x) for x in [
-                "<b><code %s>%s</code></b></br>%s%s%s" % ("style='color:#fff; background-color:#dc3545'" if vp["required"] else "", vp["name"], vp["type"], ", <i>required</i>" if vp["required"] else "", ", <i>enum</i>" if len(vp.get("enum")) > 0 else ""),
+                name + '</br>' + field_description,
                 #vp["path"],
-                vp.get("description", "").replace('\n', "</br>"),
+                vp.get("description", "").replace('\n', "</br>") + aside,
                 "<ul>" + ''.join(["<li><code>%s</code></li>" % f for f in vp.get("enum", "")]) + "</ul>",
             ]]))
 
+    print("\n\n### Metrics")
 
+    cmd_metric = []
+    uploader_map = {}
+    for kp, vp in sort_flat:
+        if "metric" not in vp["path"]:
+            continue
+        if vp.get("x-ocarina-param") == "NA":
+            pass
+        else:
+            example = vp.get("example")
+            if ' ' in vp.get("example"):
+                example = "'%s'" % example
+            cmd_metric.append("\t--metric %s%s %s %s \\" % (vp.get("x-ocarina-namespace"), ".#" if vp.get("x-ocarina-in-record") else "", vp.get("x-ocarina-param"), example))
+
+        if vp.get("x-uploader-column"):
+            uploader_map["%s %s" % (vp.get("x-ocarina-namespace"), vp.get("x-ocarina-param"))] = "%s%s" % (vp.get("x-uploader-column"), " (limit %d)" % vp.get("x-uploader-limit") if vp.get("x-uploader-limit") else "")
+
+    if spec["post"].get("x-ocarina-cmd", ""):
+        if spec["post"]["x-ocarina-cmd"]:
+            if len(cmd_metric) > 0:
+                cmd_metric[-1] = cmd_metric[-1][:-1]
+            print("""<blockquote class="lang-specific shell--ocarina"><p>To provide metrics with Ocarina:</p></blockquote>""")
+            print("```shell--ocarina")
+            print(cmd_head + ' \\' )
+            print("\t...")
+            print('\n'.join(cmd_metric))
+            print("```")
+            print("""<blockquote class="lang-specific shell--ocarina"><p>If a particular metric supports storing multiple records, you can provide them by incrementing a numerical suffix after the metric's namespace: <i>e.g.</i> <code>--metric name.1 key value</code> ... <code>--metric name.N key value.</code></blockquote>""")
+
+    if len(uploader_map) > 0:
+        print("""<blockquote class="lang-specific plaintext--uploader"><p>Some metrics can be provided via the uploader using these column names:</p>""")
+        print("<ul>%s</ul>" % '\n'.join(["<li><code>%s</code> ▶ <code>%s</code></li>" % (k, v) for k, v in uploader_map.items()]))
+        print("</blockquote>")
+
+
+
+    print("\nNamespace | Name | Description | Options")
+    print("--- | ---- | ----------- | -------")
+    for kp, vp in sort_flat:
+        if "metric" not in vp["path"]:
+            continue
+        print(' | '.join([str(x) for x in [
+            "<b><code>%s</code></b>" % vp["x-ocarina-namespace"],
+            "<b><code>%s</code></b>" % vp["name"],
+            vp.get("description", "").replace('\n', "</br>"),
+            "<ul>" + ''.join(["<li><code>%s</code></li>" % f for f in vp.get("enum", "")]) + "</ul>",
+        ]]))
+
+    print("\n\n### Metadata\n")
+
+    cmd_meta = []
+    uploader_map = {}
+    for kp, vp in sort_flat:
+        if "metadata" not in vp["path"]:
+            continue
+        if vp.get("x-ocarina-param") == "NA":
+            pass
+        else:
+            example = vp.get("example")
+            if ' ' in vp.get("example"):
+                example = "'%s'" % example
+            cmd_meta.append("\t-m %s %s %s \\" % (vp.get("x-ocarina-namespace"), vp.get("x-ocarina-param"), example))
+
+        if vp.get("x-uploader-column"):
+            uploader_map["%s %s" % (vp.get("x-ocarina-namespace"), vp.get("x-ocarina-param"))] = "%s%s" % (vp.get("x-uploader-column"), " (limit %d)" % vp.get("x-uploader-limit") if vp.get("x-uploader-limit") else "")
+
+    if spec["post"].get("x-ocarina-cmd", ""):
+        if spec["post"]["x-ocarina-cmd"]:
+            if len(cmd_meta) > 0:
+                cmd_meta[-1] = cmd_meta[-1][:-1]
+            print("""<blockquote class="lang-specific shell--ocarina"><p>To provide metadata with Ocarina:</p></blockquote>""")
+            print("```shell--ocarina")
+            print(cmd_head + ' \\' )
+            print("\t...")
+            print('\n'.join(cmd_meta))
+            print("```")
+
+    if len(uploader_map) > 0:
+        print("""<blockquote class="lang-specific plaintext--uploader"><p>Some metadata can be provided via the uploader using these column names:</p>""")
+        print("<ul>%s</ul>" % '\n'.join(["<li><code>%s</code> ▶ <code>%s</code></li>" % (k, v) for k, v in uploader_map.items()]))
+        print("</blockquote>")
+
+    print("Any artifact in Majora can be 'tagged' with arbitrary key-value metadata.")
+    print("There is no limit or validation on the keys or their values. To aid organisation, keys are grouped into namespaces.")
+    print("You should note the following keys are 'reserved' and should only be used to provide meaningful information:")
+
+    print("\nNamespace | Name | Description | Options")
+    print("--- | ---- | ----------- | -------")
+    for kp, vp in sort_flat:
+        if "metadata" not in vp["path"]:
+            continue
+        print(' | '.join([str(x) for x in [
+            "<b><code>%s</code></b>" % vp["x-ocarina-namespace"],
+            "<b><code>%s</code></b>" % vp["name"],
+            vp.get("description", "").replace('\n', "</br>"),
+            "<ul>" + ''.join(["<li><code>%s</code></li>" % f for f in vp.get("enum", "")]) + "</ul>",
+        ]]))
+
+
+
+    print("\n\n")
